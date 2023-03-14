06b146948b1b5cf96e3f4c12f3bdbe63521ab867
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-03-14 21:04:03 +0200

    created form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Section } from 'components/Section/Section';
+import { AddContactsForm } from 'components/AddContactsForm/AddContactsForm';
+// import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+// import { Statistics } from 'components/Statistics/Statistics';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  formSubmit = data => {
+    console.log(this.state);
+    // const isMatch = this.state.contacts.find(
+    //   contact => contact.name.toLowerCase() === data.name.toLowerCase()
+    // );
+  }
+  render() {
+    return (
+      <>
+        <Section title="Phonebook">
+          <AddContactsForm formSubmit={()=>this.formSubmit()}/>
+        </Section>
+        <Section title="Contacts"></Section>
+      </>
+    );
+  }
+}
